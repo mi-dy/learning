53c7c42101ceae6f7a14ba22a10baedd5c129bf0
Mindaugas Dyra <mindaugasdy@gmail.com>
2025-01-31 14:52:07 +0200

    Create BTC_price_check.py

---
+++ CS50/Bitcoin API/BTC_price_check.py
@@ -0,0 +1,31 @@
+import json
+import requests
+import sys
+
+def main():
+    try:
+        if len(sys.argv) != 2:
+            sys.exit("Missing command-line argument")
+        else:
+            ammount = float(sys.argv[1])
+    except ValueError:
+        sys.exit("Command-line argument is not a number")
+    price = get_price()
+    order_price(ammount, price)
+
+
+def get_price():
+    while True:
+        try:
+            bitcoin = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+            o = bitcoin.json()
+            return o["bpi"]["USD"]["rate_float"]
+        except requests.RequestException:
+            continue
+
+def order_price(ammount, price):
+    order = ammount * price
+    print(f"${order:,.4f}")
+
+if __name__ == "__main__":
+    main()
