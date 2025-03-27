b7db34ee557eb685e36df8ac2a0ce54335f4b621
Mindaugas Dyra <mindaugasdy@gmail.com>
2025-03-27 10:43:11 +0200

    Update cash_register.py

--- CS50/cash_register.py
+++ CS50/cash_register.py
@@ -2,7 +2,6 @@ def main():
     total = 0
     while True:
         try:
-            #expetcted input as written in menu, case insensitive
             x = input_f()
             y = tab(x)
             if y == 0:
@@ -10,15 +9,14 @@ def main():
             else:
                 total = total + y
                 print(f"Total: ${total:.2f}")
-        #continues repetition of main program until "ctrl + d" input
         except EOFError:
             print("")
             break
-#formatting input
+
 def input_f():
     x = input("Item: ").lower()
     return x.title()
-#getting the price of the selected item or 0 if item does not exist
+
 def tab(x):
     menu = {
     "Baja Taco": 4.25,
