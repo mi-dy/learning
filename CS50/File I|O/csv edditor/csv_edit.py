5a2c7c7e5748fefe987be13146ebf1993cf4e076
Mindaugas Dyra <mindaugasdy@gmail.com>
2025-03-27 10:40:22 +0200

    Update csv_edit.py

--- CS50/File I|O/csv edditor/csv_edit.py
+++ CS50/File I|O/csv edditor/csv_edit.py
@@ -3,7 +3,6 @@ import csv
 
 def main():
     try:
-      #expecting 2 arguments in command line, first for file to convert, second for output
         if len(sys.argv) != 3:
             sys.exit("Argument count is not 2")
         else:
@@ -11,13 +10,13 @@ def main():
             converter(sys.argv[1], sys.argv[2])
     except FileNotFoundError:
         sys.exit("File not found")
-#checking if expected lines are in the files fieldnames
+
 def file_check(x):
     with open(x, "r") as file:
         reader = csv.DictReader(file)
         if "name" not in reader.fieldnames or"house" not in reader.fieldnames:
             sys.exit("Wrong file format")
-#adding aditional fieldname. Instead of Name, now we have First and Last names
+
 def converter(a, b):
     with open(a, "r") as file1, open(b, "w") as file2:
         csv.writer(file2).writerow(["first","last","house"])
