7b8c033cab97a348da4e8e14e9f3bb4470f564e5
Mindaugas <mindaugasdy@gmail.com>
2025-04-03 12:15:51 +0300

    Added choice to move first or second

--- Tic-Tac-Toe/tic-tac-toe.py
+++ Tic-Tac-Toe/tic-tac-toe.py
@@ -1,19 +1,47 @@
 from tkinter import *
 
-def click(b):
-    #If current player 1 print X else print O
-    b.config(text="X")
+def choose_sequence():
 
-if __name__ == "__main__":
-    window = Tk()
-    buttons = []
+    window.title("Tic-Tac-Toe Choose!")
+
+    label = Label(window,
+                  text="Choose to go first or second?",
+                  font=("Arial",20,"bold"),
+                  relief=SUNKEN,
+                  bd=5,
+                  padx=5,
+                  pady=5)
+    label.pack()
+
+    button_first = Button(window,text="First!",command=lambda: game(window, "X"))
+    button_first.pack()
+
+    button_second = Button(window,text="Second!",command=lambda: game(window, "O"))
+    button_second.pack()
+
+
+def game(window, player):
+    for widget in window.winfo_children():
+        widget.destroy()
+
+    window.title("Tic-Tac-Toe Play!")
 
     for n in range(9):
         button = Button(window)
-        button.config(command=lambda b=button: click(b))
+        button.config(command=lambda b=button: click(b, player))
         row, column = divmod(n, 3)
         button.grid(row=row, column=column)
 
+def click(b, player):
+    b.config(text=player)
+
+
+if __name__ == "__main__":
+    window = Tk()
+    window.geometry("400x400")
+
+    choose_sequence()
+    
     window.mainloop()
     #User chooses if he moves first or second. First player is assigned X as button values, second - O
     #If bot goes first, picks middle button.
